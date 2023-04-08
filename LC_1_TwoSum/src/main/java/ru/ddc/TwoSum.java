fd2a9ede5c46cb4dffefbab6b92644ac997a8a3e
Sergey Kovalev <dipdeepcode@mail.ru>
2023-04-09 00:09:57 +0300

    LC_1_TwoSum optimized solution without HashMap

--- LC_1_TwoSum/src/main/java/ru/ddc/TwoSum.java
+++ LC_1_TwoSum/src/main/java/ru/ddc/TwoSum.java
@@ -1,22 +1,64 @@
 package ru.ddc;
 
 public class TwoSum {
+    private Node[] arr;
 
     public int[] twoSum(int[] nums, int target) {
-        int length = nums.length;
-        Node[] arr = new Node[length];
-        for (int i = 0; i < length; i++) {
+        arr = new Node[nums.length];
+        for (int i = 0; i < nums.length; i++) {
             int key = target - nums[i];
-            int index = Math.abs(key % length);
-            if (arr[index] != null) {
-                return new int[]{arr[index].value, i};
+            if (getNodeForKey(key) != null) {
+                return new int[]{get(key), i};
             }
-            arr[Math.abs(nums[i] % length)] = new Node(nums[i], i);
+            put(nums[i], i);
         }
         return null;
     }
 
+    public void put(int key, int value) {
+
+        Node node = getNodeForKey(key);
+        if (node != null) {
+            node.value = value;
+        } else {
+            node = new Node(key, value);
+            int index = getIndexForKey(key);
+            if (arr[index] != null) {
+                node.next = arr[index];
+                node.next.prev = node;
+            }
+            arr[index] = node;
+        }
+    }
+
+    public int get(int key) {
+        Node node = getNodeForKey(key);
+        if (node != null) {
+            return node.value;
+        } else {
+            return -1;
+        }
+    }
+
+    private Node getNodeForKey(int key) {
+        int index = getIndexForKey(key);
+        Node current = arr[index];
+        while (current != null) {
+            if (current.key == key) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    public int getIndexForKey(int key) {
+        return Math.abs(key % arr.length);
+    }
+
     private static class Node {
+        public Node next;
+        public Node prev;
         public int key;
         public int value;
 
