40d69d771f94d66eebc739cfff28037c12f42538
Sergey Kovalev <dipdeepcode@mail.ru>
2023-04-08 18:28:53 +0300

    LC_67_AddBinary optimized solution

--- LC_1_TwoSum/src/main/java/ru/ddc/TwoSum.java
+++ LC_1_TwoSum/src/main/java/ru/ddc/TwoSum.java
@@ -1,12 +1,17 @@
 package ru.ddc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if ((nums[i] + nums[j]) == target) return new int[]{i, j};
+        Map<Integer, Integer> proxy = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (proxy.containsKey(target - nums[i])) {
+                return new int[]{proxy.get(target - nums[i]), i};
             }
+            proxy.put(nums[i], i);
         }
-        return null;
+        return new int[] {};
     }
 }
