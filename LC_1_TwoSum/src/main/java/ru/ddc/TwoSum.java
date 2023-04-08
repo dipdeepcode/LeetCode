f36efed0e16825b44e3dd0ebfc7b4abffc292f79
Sergey Kovalev <dipdeepcode@mail.ru>
2023-04-08 23:03:52 +0300

    LC_67_AddBinary optimized solution without HashMap

--- LC_1_TwoSum/src/main/java/ru/ddc/TwoSum.java
+++ LC_1_TwoSum/src/main/java/ru/ddc/TwoSum.java
@@ -1,17 +1,28 @@
 package ru.ddc;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TwoSum {
+
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> proxy = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (proxy.containsKey(target - nums[i])) {
-                return new int[]{proxy.get(target - nums[i]), i};
+        int length = nums.length;
+        Node[] arr = new Node[length];
+        for (int i = 0; i < length; i++) {
+            int key = target - nums[i];
+            int index = Math.abs(key % length);
+            if (arr[index] != null) {
+                return new int[]{arr[index].value, i};
             }
-            proxy.put(nums[i], i);
+            arr[Math.abs(nums[i] % length)] = new Node(nums[i], i);
         }
-        return new int[] {};
+        return null;
+    }
+
+    private static class Node {
+        public int key;
+        public int value;
+
+        public Node(int k, int v) {
+            key = k;
+            value = v;
+        }
     }
 }
