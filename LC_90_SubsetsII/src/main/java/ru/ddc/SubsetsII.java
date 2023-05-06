ac8e6fc90851f4236ec4a93d4e8753ebf68c2aa3
Sergey Kovalev <dipdeepcode@mail.ru>
2023-05-06 16:02:49 +0300

    LC_90_SubsetII быстрое решение

--- LC_90_SubsetsII/src/main/java/ru/ddc/SubsetsII.java
+++ LC_90_SubsetsII/src/main/java/ru/ddc/SubsetsII.java
@@ -5,18 +5,22 @@ import java.util.*;
 public class SubsetsII {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> result = new HashSet<>();
-        result.add(new ArrayList<>());
+        List<List<Integer>> result = new ArrayList<>(List.of(new ArrayList<>()));
+        int end = 0;
         for (int i = 0; i < nums.length; i++) {
+            int start = 0;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                start = end + 1;
+            }
+            end = result.size() - 1;
             List<List<Integer>> temp = new ArrayList<>();
-            int finalI = i;
-            result.forEach(integers -> {
-                List<Integer> internal = new ArrayList<>(integers);
-                internal.add(nums[finalI]);
+            for (int j = start; j < result.size(); j++) {
+                List<Integer> internal = new ArrayList<>(result.get(j));
+                internal.add(nums[i]);
                 temp.add(internal);
-            });
+            }
             result.addAll(temp);
         }
-        return new ArrayList<>(result);
+        return result;
     }
 }
