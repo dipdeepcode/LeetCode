0c7047882f47ba42526ae72cc355192eba590772
Sergey Kovalev <dipdeepcode@mail.ru>
2023-08-18 18:15:42 +0300

    LC_7_ReverseInteger :: быстрое решение

--- LC_7_ReverseInteger/src/main/java/ru/ddc/ReverseInteger.java
+++ LC_7_ReverseInteger/src/main/java/ru/ddc/ReverseInteger.java
@@ -3,23 +3,15 @@ package ru.ddc;
 public class ReverseInteger
 {
     public int reverse(int x) {
-        StringBuilder sb = new StringBuilder(String.valueOf(x));
-        boolean flag = false;
-        if (sb.charAt(0) == '-') {
-            sb.deleteCharAt(0);
-            flag = true;
-        }
-        sb.reverse();
-        int result;
-        try {
-            result = Integer.parseInt(sb.toString());
-            if (flag) {
-                result = -result;
+        int result = 0;
+        while (x != 0) {
+            if (result * 10 / 10 != result) {
+                return 0;
+            } else {
+                result = result * 10 + x % 10;
+                x = x / 10;
             }
-        } catch (Exception e) {
-            return 0;
         }
-
         return result;
     }
 }
