5f77be1ad90539a939e028b569253f71acf10edd
Sergey Kovalev <dipdeepcode@mail.ru>
2023-08-25 19:05:18 +0300

    LC_9_PalindromeNumber :: быстрое решение

--- LC_9_PalindromeNumber/src/main/java/ru/ddc/PalindromeNumber.java
+++ LC_9_PalindromeNumber/src/main/java/ru/ddc/PalindromeNumber.java
@@ -1,20 +1,11 @@
 package ru.ddc;
 
 public class PalindromeNumber {
-    public boolean isPalindrome(int x) {
-
-        if (x < 0) {
-            return false;
+    public boolean isPalindrome(final int x) {
+        int reverse = 0;
+        for (int origin = x; origin >= 1; origin = origin / 10) {
+            reverse = reverse * 10 + origin % 10;
         }
-
-        String s = String.valueOf(x);
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                return false;
-            }
-        }
-
-        return true;
+        return x == reverse;
     }
 }
-
