a36e37d6cda622b8a52ef1f5d4daf1a9df940d2a
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-06 21:15:38 +0300

    LC_0012 :: быстрое решение

--- LC_0012_IntegerToRoman/src/main/java/ru/ddc/IntegerToRoman.java
+++ LC_0012_IntegerToRoman/src/main/java/ru/ddc/IntegerToRoman.java
@@ -1,49 +1,17 @@
 package ru.ddc;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class IntegerToRoman {
+    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
     public String intToRoman(int num) {
-        Map<Integer, String> integerRomanMap = new HashMap<>() {{
-            put(0, "");
-            put(1, "I");
-            put(5, "V");
-            put(10, "X");
-            put(50, "L");
-            put(100, "C");
-            put(500, "D");
-            put(1000, "M");
-        }};
-
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; num > 0; i++) {
-            int x = num % 10;
-
-            if (x <= 3) {
-                for (int j = 0; j < x; j++) {
-                    sb.append(integerRomanMap.get((int) Math.pow(10, i)));
-                }
+        for (int i = 0; i < values.length && num > 0; i++) {
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
             }
-            if (x == 4) {
-                sb.append(integerRomanMap.get((int) Math.pow(10, i) * 5))
-                        .append(integerRomanMap.get((int) Math.pow(10, i)));
-            }
-            if (x >= 5 && x < 9) {
-                for (int j = 5; j < x; j++) {
-                    sb.append(integerRomanMap.get((int) Math.pow(10, i)));
-                }
-                sb.append(integerRomanMap.get((int) Math.pow(10, i) * 5));
-            }
-            if (x == 9) {
-                sb.append(integerRomanMap.get((int) Math.pow(10, i) * 10));
-                sb.append(integerRomanMap.get((int) Math.pow(10, i)));
-            }
-
-            num = num / 10;
-
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
