ffae716524f3f0865936f9ce82064112b3270211
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-11 21:58:42 +0300

    LC_0014 :: Быстрое решение

--- LC_0014_LongestCommonPrefix/src/main/java/ru/ddc/LongestCommonPrefix.java
+++ LC_0014_LongestCommonPrefix/src/main/java/ru/ddc/LongestCommonPrefix.java
@@ -2,34 +2,14 @@ package ru.ddc;
 
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-
-        return CommonPrefix(strs, 0, strs.length - 1);
-    }
-
-    private String CommonPrefix(String[] strs, int start, int end)
-    {
-        if (start == end)
-            return strs[start];
-
-        int middle = (start + end) / 2;
-
-        var left = CommonPrefix(strs, start, middle);
-        var right = CommonPrefix(strs, middle + 1, end);
-        return ConquerString(left, right);
-    }
-
-    private String ConquerString(String left, String right)
-    {
-        int currentSmallest = Math.min(left.length(), right.length());
-        for (int i = 0; i < currentSmallest; i++)
-        {
-            if (left.charAt(i) != right.charAt(i)) {
-                return left.substring(0,i);
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(strs[0])) {
+                strs[0] = strs[0].substring(0, strs[0].length() - 1);
+            }
+            if (strs[0].isEmpty()) {
+                break;
             }
         }
-        return left.substring(0, currentSmallest);
+        return strs[0];
     }
 }
