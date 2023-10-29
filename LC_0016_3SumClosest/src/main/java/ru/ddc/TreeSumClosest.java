b2b4d27e2aeedeca99f772740a84ba58a56c1e35
Sergey Kovalev <dipdeepcode@mail.ru>
2023-10-29 17:26:40 +0300

    LC_0016_3SumClosest :: Быстрое решение

--- LC_0016_3SumClosest/src/main/java/ru/ddc/TreeSumClosest.java
+++ LC_0016_3SumClosest/src/main/java/ru/ddc/TreeSumClosest.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 public class TreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
+        int len = nums.length;
+        if (nums[len - 3] + nums[len - 2] + nums[len - 1] <= target){
+            return nums[len - 3] + nums[len - 2] + nums[len - 1];
+        }
+        if (nums[0] + nums[1] + nums[2] >= target){
+            return nums[0] + nums[1] + nums[2];
+        }
         int result = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length - 2; i++) {
             int l = i + 1;
