bdcee63c1ba3ea4e51396b9562b7c666368cd5b2
Sergey Kovalev <dipdeepcode@mail.ru>
2023-12-17 22:52:06 +0300

    LC_0031_NextPermutation :: Быстрое решение

--- LC_0031_NextPermutation/src/main/java/ru/ddc/NextPermutation.java
+++ LC_0031_NextPermutation/src/main/java/ru/ddc/NextPermutation.java
@@ -1,28 +1,42 @@
 package ru.ddc;
 
-import java.util.Arrays;
-
 public class NextPermutation {
     public void nextPermutation(int[] nums) {
-        int index1 = -1;
-        for (int i = 0; i < nums.length - 1; i++) {
+        int ind1 = -1;
+        int ind2 = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                index1 = i;
+                ind1 = i;
+                break;
             }
         }
-        if (index1 == -1) {
-            Arrays.sort(nums);
-            return;
-        }
-        int index2 = index1 + 1;
-        for (int i = index2; i < nums.length; i++) {
-            if (nums[i] >  nums[index1] && nums[i] < nums[index2]) {
-                index2 = i;
+        if (ind1 == -1) {
+            reverse(nums, 0);
+        } else {
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (nums[i] > nums[ind1]) {
+                    ind2 = i;
+                    break;
+                }
             }
+            swap(nums, ind1, ind2);
+            reverse(nums, ind1 + 1);
         }
-        int x = nums[index1];
-        nums[index1] = nums[index2];
-        nums[index2] = x;
-        Arrays.sort(nums, index1 + 1, nums.length);
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    void reverse(int[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
     }
 }
