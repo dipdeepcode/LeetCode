36d7e28312d1184b8a8f7f231a1331e2a4482ae4
Sergey Kovalev <dipdeepcode@mail.ru>
2024-01-06 15:33:27 +0300

    LC_0019_RemoveNthNodeFromEndOfList :: Быстрое решение

--- LC_0019_RemoveNthNodeFromEndOfList/src/main/java/ru/ddc/RemoveNthNodeFromEndOfList.java
+++ LC_0019_RemoveNthNodeFromEndOfList/src/main/java/ru/ddc/RemoveNthNodeFromEndOfList.java
@@ -1,46 +1,29 @@
 package ru.ddc;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class RemoveNthNodeFromEndOfList {
-    private final Deque<ListNode> deque = new LinkedList<>();
-
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        initDeque(n);
-
-        int listSize = 1;
-        ListNode currNode = head;
-        while (currNode.next != null) {
-            addNode(currNode);
-            currNode = currNode.next;
-            listSize++;
+        if (head.next == null) {
+            return null;
         }
 
-        if (listSize == n) {
-            if (listSize == 1) {
-                head = null;
-            } else {
-                addNode(null);
-                head = deque.getFirst().next;
-            }
-        } else if (n == 1) {
-            deque.getFirst().next = null;
-        } else {
-            deque.getFirst().next = deque.getFirst().next.next;
+        int size = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            size++;
         }
 
+        if (n == size) {
+            return head.next;
+        }
+
+        temp = head;
+        int currp = 1;
+        while (currp != size - n) {
+            temp = temp.next;
+            currp++;
+        }
+        temp.next = temp.next.next;
         return head;
-    }
-
-    private void initDeque(int n) {
-        for (int i = 0; i < n; i++) {
-            deque.add(null);
-        }
-    }
-
-    private void addNode(ListNode node) {
-        deque.removeFirst();
-        deque.addLast(node);
     }
 }
