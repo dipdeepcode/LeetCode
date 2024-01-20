f2eedf7f730e7adf5e609d31601c027ee849449e
Sergey Kovalev <dipdeepcode@mail.ru>
2024-01-20 14:49:43 +0300

    LC_0021_MergeTwoSortedLists :: Решение с рекурсией

--- LC_0021_MergeTwoSortedLists/src/main/java/ru/ddc/MergeTwoSortedLists.java
+++ LC_0021_MergeTwoSortedLists/src/main/java/ru/ddc/MergeTwoSortedLists.java
@@ -3,39 +3,18 @@ package ru.ddc;
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        if (list1 == null && list2 == null) return null;
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-
-        ListNode resultHead, currentNode1, currentNode2, node;
-        if (list1.val < list2.val) {
-            resultHead = list1;
-            currentNode1 = list1.next;
-            currentNode2 = list2;
-        } else {
-            resultHead = list2;
-            currentNode1 = list1;
-            currentNode2 = list2.next;
-        }
-        node = resultHead;
-
-        while (true) {
-            if (currentNode1 == null) {
-                node.next = currentNode2;
-                break;
-            } else if (currentNode2 == null) {
-                node.next = currentNode1;
-                break;
-            }
-            if (currentNode1.val < currentNode2.val) {
-                node.next = currentNode1;
-                currentNode1 = currentNode1.next;
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
             } else {
-                node.next = currentNode2;
-                currentNode2 = currentNode2.next;
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
             }
-            node = node.next;
+        } else if (list1 == null) {
+            return list2;
+        } else {
+            return list1;
         }
-        return resultHead;
     }
 }
