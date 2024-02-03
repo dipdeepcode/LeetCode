9e5bb20b3118f76c53ba16ed105b9d97d4ae7204
Sergey Kovalev <dipdeepcode@mail.ru>
2024-02-03 18:35:44 +0300

    LC_0036_ValidSudoku :: Быстрое решение

--- LC_0036_ValidSudoku/src/main/java/ru/ddc/ValidSudoku.java
+++ LC_0036_ValidSudoku/src/main/java/ru/ddc/ValidSudoku.java
@@ -1,37 +1,26 @@
 package ru.ddc;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-
         for (int i = 0; i < 9; i++) {
-            Set<Character> x_characters = new HashSet<>();
-            Set<Character> y_characters = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.' && !x_characters.add(board[i][j])) {
-                    return false;
-                }
-                if (board[j][i] != '.' && !y_characters.add(board[j][i])) {
-                    return false;
-                }
-            }
-        }
+                if (board[i][j] == '.') continue;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Set<Character> characters = new HashSet<>();
-                for (int k = i * 3; k < i * 3 + 3; k++) {
-                    for (int l = j * 3; l < j * 3 + 3; l++) {
-                        if (board[k][l] != '.' && !characters.add(board[k][l])) {
-                            return false;
-                        }
+                for (int z = i / 3 * 3; z < i / 3 * 3 + 3; z++) {
+                    for (int y = j / 3 * 3; y < j / 3 * 3 + 3; y++) {
+                        if (board[z][y] == board[i][j] && z != i && y != j) return false;
                     }
                 }
+
+                for (int k = j + 1; k < 9; k++) {
+                    if (board[i][j] == board[i][k]) return false;
+                }
+
+                for (int k = i + 1; k < 9; k++) {
+                    if (board[i][j] == board[k][j]) return false;
+                }
             }
         }
-
         return true;
     }
 }
