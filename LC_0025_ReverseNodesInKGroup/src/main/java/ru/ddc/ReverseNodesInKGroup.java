a0125d2091c46c97c37e5e38b47a1f9e28d5850c
Sergey Kovalev <dipdeepcode@mail.ru>
2024-02-24 11:39:18 +0300

    LC_0025_ReverseNodesInKGroup :: Компактное решение

--- LC_0025_ReverseNodesInKGroup/src/main/java/ru/ddc/ReverseNodesInKGroup.java
+++ LC_0025_ReverseNodesInKGroup/src/main/java/ru/ddc/ReverseNodesInKGroup.java
@@ -2,51 +2,35 @@ package ru.ddc;
 
 public class ReverseNodesInKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
-
-        ListNode result = null;
-        ListNode oldGroupHead = null;
-
-        ListNode previousNode = null;
-        ListNode currentNode = null;
-        ListNode nextNode = null;
-
-        ListNode startNodeInGroup = head;
-        for (int group = 0; hasFullGroupFromStartNode(startNodeInGroup, k); group++) {
-
-
-            previousNode = null;
-            currentNode = startNodeInGroup;
-            nextNode = currentNode.next;
-            for (int i = 0; i < k; i++) {
-                currentNode.next = previousNode;
-                previousNode = currentNode;
-                currentNode = nextNode;
-                nextNode = currentNode == null ? null : currentNode.next;
-            }
-            if (group == 0) {
-                result = previousNode;
-            }
-
-            if (oldGroupHead != null) {
-                oldGroupHead.next = previousNode;
-            }
-
-            oldGroupHead = startNodeInGroup;
-            startNodeInGroup = currentNode;
-
+        ListNode dummy = new ListNode(0, head);
+        ListNode startNode = dummy;
+        while (startNode != null) {
+            startNode = reverseKNodes(startNode, k);
         }
-        if (oldGroupHead != null) {
-            oldGroupHead.next = currentNode;
-        }
-
-        return result;
+        return dummy.next;
     }
-
-    private boolean hasFullGroupFromStartNode(ListNode node, int k) {
-        if (node != null && k > 1) {
-            return hasFullGroupFromStartNode(node.next, k - 1);
-        } else {
-            return node != null;
+    public ListNode reverseKNodes(ListNode startNode, int k) {
+        ListNode groupHeadNode = startNode.next;
+        ListNode currentNode = startNode.next;
+        ListNode previousNode = startNode.next;
+        for (int i = 0; i < k; i++) {
+            if (previousNode == null) {
+                return null;
+            } else {
+                previousNode = previousNode.next;
+            }
         }
+        ListNode nextNode;
+
+        for (int j = 0; j < k; j++) {
+            nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        startNode.next = previousNode;
+
+        return groupHeadNode;
     }
 }
