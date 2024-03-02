b268ad31af0a4b0f4c35cae0d2e8d12aa54f5853
Sergey Kovalev <dipdeepcode@mail.ru>
2024-03-02 12:59:18 +0300

    LC_0028_FindTheIndexOfTheFirstOccurrenceInAString :: Быстрое решение без методов класса String

--- LC_0028_FindTheIndexOfTheFirstOccurrenceInAString/src/main/java/ru/ddc/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ LC_0028_FindTheIndexOfTheFirstOccurrenceInAString/src/main/java/ru/ddc/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -2,6 +2,20 @@ package ru.ddc;
 
 public class FindTheIndexOfTheFirstOccurrenceInAString {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int m = haystack.length();
+        int n = needle.length();
+        int ans = 0;
+        while (ans <= m - n) {
+            for (int i = 0; i < n; i++) {
+                if (needle.charAt(i) != haystack.charAt(i + ans)) {
+                    ans++;
+                    break;
+                }
+                if (i == n - 1) {
+                    return ans;
+                }
+            }
+        }
+        return -1;
     }
 }
