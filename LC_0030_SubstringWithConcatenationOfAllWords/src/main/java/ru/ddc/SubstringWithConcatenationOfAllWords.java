6bc2d4f17941cfa6805b2d5d6f19f985a5d43652
Sergey Kovalev <dipdeepcode@mail.ru>
2024-03-16 23:36:29 +0300

    LC_0030_SubstringWithConcatenationOfAllWords :: Быстрое решение

--- LC_0030_SubstringWithConcatenationOfAllWords/src/main/java/ru/ddc/SubstringWithConcatenationOfAllWords.java
+++ LC_0030_SubstringWithConcatenationOfAllWords/src/main/java/ru/ddc/SubstringWithConcatenationOfAllWords.java
@@ -1,65 +1,29 @@
 package ru.ddc;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SubstringWithConcatenationOfAllWords {
+
     public List<Integer> findSubstring(String s, String[] words) {
 
-        int wordsLength = words[0].length();
-        String[] strings = s.split("(?<=\\G.{" + wordsLength + "})");
-        System.out.println(s);
-        System.out.println(Arrays.toString(strings));
+        HashMap<String, Integer> counts = new HashMap<>() {{
+            for (String word : words) merge(word, 1, Integer::sum);
+        }};
 
-        Cell[][] dp = new Cell[words.length][strings.length];
-        for (int i = 0; i < words.length; i++) {
-            for (int j = i; j < strings.length; j++) {
-                if (i == 0) {
-                    dp[i][j] = new Cell(words, -1);
-                    if (dp[i][j].removeElement(strings[j])) {
-                        dp[i][j].setIndex(j);
-                    }
-                } else {
-                    if (dp[i - 1][j - 1] != null && dp[i - 1][j - 1].removeElement(strings[j])) {
-                        dp[i][j] = dp[i - 1][j - 1];
+        List<Integer> result = new ArrayList<>();
+        int numOfWords = words.length;
+        int wordLength = words[0].length();
+        for (int i = 0; i <= s.length() - numOfWords * wordLength; i++)
+            abc:{
+                Map<String, Integer> countsCopy = (Map<String, Integer>) counts.clone();
+                for (int j = 0; j < numOfWords; j++) {
+                    String word = s.substring(i + j * wordLength, i + (j + 1) * wordLength);
+                    if (countsCopy.merge(word, -1, Integer::sum) < 0) {
+                        break abc;
                     }
                 }
-                System.out.print(dp[i][j] + " ");
+                result.add(i);
             }
-            System.out.println();
-        }
-
-        return Arrays.stream(dp[words.length - 1])
-                .filter(cell -> cell != null && cell.getIndex() >= 0)
-                .map(cell -> cell.getIndex() * wordsLength)
-                .toList();
-    }
-
-    private static class Cell {
-        List<String> elements;
-        int index;
-
-        public Cell(String[] words, int index) {
-            this.elements = new ArrayList<>(Arrays.asList(words));
-            this.index = index;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public void setIndex(int index) {
-            this.index = index;
-        }
-
-        public boolean removeElement(String element) {
-            return elements.remove(element);
-        }
-
-        @Override
-        public String toString() {
-            return elements.toString() + index;
-        }
+        return result;
     }
 }
--- LC_0030_SubstringWithConcatenationOfAllWords/src/test/java/ru/ddc/SubstringWithConcatenationOfAllWordsTest.java
+++ LC_0030_SubstringWithConcatenationOfAllWords/src/test/java/ru/ddc/SubstringWithConcatenationOfAllWordsTest.java
@@ -49,4 +49,35 @@ class SubstringWithConcatenationOfAllWordsTest {
                 new String[]{"fooo","barr","wing","ding","wing"});
         assertIterableEquals(expected, actual);
     }
+
+    @Test
+    public void test5() {
+        List<Integer> expected = new ArrayList<>(){{
+            add(0);
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+            add(6);
+            add(7);
+            add(8);
+            add(9);
+            add(10);
+        }};
+        List<Integer> actual = substring.findSubstring("aaaaaaaaaaaaaa", new String[]{"aa","aa"});
+        assertIterableEquals(expected, actual);
+    }
+
+    @Test
+    public void test6() {
+        List<Integer> expected = new ArrayList<>(){{
+            add(0);
+            add(3);
+            add(6);
+            add(15);
+        }};
+        List<Integer> actual = substring.findSubstring("foobarfoobarthefoobarman", new String[]{"foo","bar"});
+        assertIterableEquals(expected, actual);
+    }
 }
