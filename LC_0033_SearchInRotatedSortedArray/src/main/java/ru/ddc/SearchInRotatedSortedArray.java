90f1556f6066428abca01ae110df6d01b8b32019
Sergey Kovalev <dipdeepcode@mail.ru>
2024-04-02 14:23:05 +0300

    LC_0033_SearchInRotatedSortedArray :: Решение очень грубой силой

---
+++ LC_0033_SearchInRotatedSortedArray/src/main/java/ru/ddc/SearchInRotatedSortedArray.java
@@ -0,0 +1,12 @@
+package ru.ddc;
+
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
