dc0752521e56ed4b49f3bf606302bf506c271d57
Sergey Kovalev <dipdeepcode@mail.ru>
2024-04-02 14:38:10 +0300

    LC_0033_SearchInRotatedSortedArray :: Быстрое решение

--- LC_0033_SearchInRotatedSortedArray/src/test/java/ru/ddc/SearchInRotatedSortedArrayTest.java
+++ LC_0033_SearchInRotatedSortedArray/src/test/java/ru/ddc/SearchInRotatedSortedArrayTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SearchInRotatedSortedArrayTest {
-    private SearchInRotatedSortedArray search = new SearchInRotatedSortedArray();
+    private final SearchInRotatedSortedArray search = new SearchInRotatedSortedArray();
 
     @Test
     public void test1() {
