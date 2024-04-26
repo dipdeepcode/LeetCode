44bd77c9d4465c04003b73972fe7d5affbf298cc
Sergey Kovalev <dipdeepcode@mail.ru>
2024-04-26 22:37:42 +0300

    LC_0037_SudokuSolver :: Очень быстрое решение

--- LC_0037_SudokuSolver/src/main/java/ru/ddc/SudokuSolver.java
+++ LC_0037_SudokuSolver/src/main/java/ru/ddc/SudokuSolver.java
@@ -1,41 +1,88 @@
 package ru.ddc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SudokuSolver {
+    private final int[] line = new int[9];
+    private final int[] column = new int[9];
+    private final int[][] block = new int[3][3];
+    private boolean valid = false;
+    private final List<int[]> spaces = new ArrayList<int[]>();
+
     public void solveSudoku(char[][] board) {
-        solve(board, 0);
-    }
+        // Preprocess the board to set initial state
+        preprocess(board);
 
-    private boolean solve(char[][] board, int s) {
-        if (s == 81) {
-            return true;
-        }
-
-        int i = s / 9;
-        int j = s % 9;
-
-        if (board[i][j] == '.') {
-            for (char ch = '1'; ch <= '9'; ch++) {
-                if (isValid(board, i, j, ch)) {
-                    board[i][j] = ch;
-                    if (solve(board, s + 1)) {
-                        return true;
+        // Iterate until the board is solved or no more modifications can be made
+        while (!valid) {
+            boolean modified = false;
+            for (int i = 0; i < 9; ++i) {
+                for (int j = 0; j < 9; ++j) {
+                    if (board[i][j] == '.') {
+                        int mask = ~(line[i] | column[j] | block[i / 3][j / 3]) & 0x1ff;
+                        if ((mask & (mask - 1)) == 0) {
+                            // If only one possibility, fill the cell
+                            int digit = Integer.bitCount(mask - 1);
+                            flip(i, j, digit);
+                            board[i][j] = (char) (digit + '0' + 1);
+                            modified = true;
+                        }
                     }
-                    board[i][j] = '.';
                 }
             }
-        } else {
-            return solve(board, s + 1);
-        }
-
-        return false;
-    }
-
-    private boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < 9; ++i) {
-            if (board[i][col] == c || board[row][i] == c || board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
-                return false;
+            // If no modifications made, break out of loop
+            if (!modified) {
+                break;
             }
         }
-        return true;
+
+        // Populate list of empty cells for DFS
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    spaces.add(new int[]{i, j});
+                }
+            }
+        }
+
+        // Start depth-first search to find solution
+        dfs(board, 0);
+    }
+
+    private void preprocess(char[][] board) {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] != '.') {
+                    int digit = board[i][j] - '0' - 1;
+                    flip(i, j, digit);
+                }
+            }
+        }
+    }
+
+    private void flip(int i, int j, int digit) {
+        line[i] ^= (1 << digit);
+        column[j] ^= (1 << digit);
+        block[i / 3][j / 3] ^= (1 << digit);
+    }
+
+    private void dfs(char[][] board, int pos) {
+        if (pos == spaces.size()) {
+            valid = true;
+            return;
+        }
+
+        int[] space = spaces.get(pos);
+        int i = space[0], j = space[1];
+        int mask = ~(line[i] | column[j] | block[i / 3][j / 3]) & 0x1ff;
+        for (; mask != 0 && !valid; mask &= (mask - 1)) {
+            int digitMask = mask & (-mask);
+            int digit = Integer.bitCount(digitMask - 1);
+            flip(i, j, digit);
+            board[i][j] = (char) (digit + '0' + 1);
+            dfs(board, pos + 1);
+            flip(i, j, digit);
+        }
     }
 }
