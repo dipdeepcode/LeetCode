d2d8a7283b3756fa00059a3c1cb399b3961b93c3
Sergey Kovalev <dipdeepcode@mail.ru>
2024-05-19 13:21:54 +0300

    LC_0039_CombinationSum :: Решение с рекурсией

--- LC_0039_CombinationSum/src/main/java/CombinationSum.java
+++ LC_0039_CombinationSum/src/main/java/CombinationSum.java
@@ -1,22 +1,24 @@
 import java.util.*;
 
 public class CombinationSum {
+    private final List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(result, new LinkedList<>(), candidates, target, 0);
+        dfs(new LinkedList<>(), candidates, target, 0);
         return result;
     }
-    private void dfs(List<List<Integer>> result, LinkedList<Integer> list, int[] candidates, int target, int start) {
+    private void dfs(LinkedList<Integer> list, int[] candidates, int target, int start) {
         if (target == 0) {
             result.add(new LinkedList<>(list));
-            return;
-        }
-        for (int i = start; i < candidates.length; i++) {
-            if (candidates[i] <= target) {
-                list.addFirst(candidates[i]);
-                dfs(result, list, candidates, target - candidates[i], i);
-                list.removeFirst();
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                if (candidates[i] <= target) {
+                    list.addLast(candidates[i]);
+                    dfs(list, candidates, target - candidates[i], i);
+                    list.removeLast();
+                } else {
+                    break;
+                }
             }
         }
     }
