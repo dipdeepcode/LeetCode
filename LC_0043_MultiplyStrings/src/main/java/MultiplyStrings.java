80159ec3970e4243721102c093c0296d0baba70b
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-09 11:47:47 +0300

    LC_0043_MultiplyStrings :: Быстрое решение

--- LC_0043_MultiplyStrings/src/main/java/MultiplyStrings.java
+++ LC_0043_MultiplyStrings/src/main/java/MultiplyStrings.java
@@ -4,36 +4,26 @@ public class MultiplyStrings {
             return "0";
         }
 
-        int arrLength = num1.length() + num2.length();
-        int[][] array = new int[arrLength][arrLength];
-
-        for (int i = 0; i < num2.length(); i++) {
-            for (int j = 0; j < num1.length(); j++) {
-                int n1 = num1.charAt(num1.length() - j - 1) - '0';
-                int n2 = num2.charAt(i) - '0';
-                array[j + i][num1.length() - j + i] = (n1 * n2) % 10;
-                array[j + i][num1.length() - j + i - 1] = (n1 * n2) / 10;
+        int num1Length = num1.length();
+        int num2Length = num2.length();
+        int[] prod = new int[num1Length + num2Length];
+        for(int i = num1Length - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+            for(int j = num2Length - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+                prod[i + j + 1] += d1 * d2;
             }
         }
 
-        for (int i = array.length - 1; i >= 0; i--) {
-            int sum = 0;
-            for (int[] ints : array) {
-                sum += ints[i];
-            }
-            array[arrLength - 1][i] = sum % 10;
-            if (i > 0) {
-                array[arrLength - 1][i - 1] = sum / 10;
-            }
+        for(int i = prod.length - 1; i > 0; i--) {
+            prod[i - 1] += prod[i] / 10;
+            prod[i] %= 10;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arrLength; i++) {
-            if (i != 0 || array[arrLength -1][i] != 0) {
-                sb.append(array[arrLength - 1][i]);
-            }
+        StringBuilder ans = new StringBuilder();
+        for(int i = (prod[0] == 0) ? 1 : 0; i < prod.length; i++) {
+            ans.append(prod[i]);
         }
-
-        return sb.toString();
+        return ans.toString();
     }
 }
