d529b7db4efbb89db8ffb4f079452dd46f13e07a
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-15 22:23:19 +0300

    LC_0044_WildcardMatching :: Быстрое решение

--- LC_0044_WildcardMatching/src/main/java/WildcardMatching.java
+++ LC_0044_WildcardMatching/src/main/java/WildcardMatching.java
@@ -1,29 +1,34 @@
 public class WildcardMatching {
 
     public boolean isMatch(String s, String p) {
-        int pLength = p.length();
         int sLength = s.length();
-        boolean[][] dp = new boolean[pLength + 1][sLength + 1];
+        int pLength = p.length();
+        int sIndex = 0;
+        int pIndex = 0;
+        int fakePIndex = -1;
+        int fakeSIndex = -1;
 
-        dp[0][0] = true;
-
-        int sp = 0;
-        while (sp < pLength && p.charAt(sp) == '*') {
-            dp[++sp][0] = true;
-        }
-
-        for (int i = 1; i <= pLength; i++) {
-            for (int j = 1; j <= sLength; j++) {
-                if (s.charAt(j - 1) == p.charAt(i - 1) || p.charAt(i - 1) == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p.charAt(i - 1) == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = false;
-                }
+        while (sIndex < sLength) {
+            if (pIndex < pLength && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '?')) {
+                sIndex++;
+                pIndex++;
+            } else if (pIndex < pLength && p.charAt(pIndex) == '*') {
+                fakePIndex = pIndex;
+                fakeSIndex = sIndex;
+                pIndex++;
+            } else if (fakePIndex != -1) {
+                pIndex = fakePIndex + 1;
+                fakeSIndex++;
+                sIndex = fakeSIndex;
+            } else {
+                return false;
             }
         }
 
-        return dp[pLength][sLength];
+        while (pIndex < pLength && p.charAt(pIndex) == '*') {
+            pIndex++;
+        }
+
+        return pIndex == pLength;
     }
 }
